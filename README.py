917f34f54bfbeb7263214e4068a17d10f81601f3
zion-walker <74158727+zion-walker@users.noreply.github.com>
2021-06-08 13:08:28 -0400

    README updated with rough project outline

--- README.py
+++ README.py
@@ -1,1 +1,14 @@
-#HI this is a README
+# min-travelers README
+
+#import necessary packages
+#Load in the Dataset
+#Create a new table from data: columns: individual video, row: participant response
+#Determine the number of participant responses per mascot video
+#Identify the number of “Hit” per mascot video
+#Calculate the “Hit Rate” per mascot video
+#Merge mascot video HR data so that columns: mascot, row: HR
+#Calculate the average HR per mascot
+#Mixed effect model w/ HR, mascot, and condition
+
+
+
