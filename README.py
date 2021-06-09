ce45b69f09b43571b16431ccc47b9f901ed46d98
zion-walker <74158727+zion-walker@users.noreply.github.com>
2021-06-09 16:07:17 -0400

    shes very WIP right now

--- README.py
+++ README.py
@@ -1,7 +1,32 @@
 # min-travelers README
 
-#import necessary packages
-#Load in the Dataset
+# import necessary packages
+# Load the Dataset into Pandas:
+#   *Open and Read the csv file
+# Clean the Dataset:
+#   *Make column names into strings and remove white space
+#   *Create a new table with columns only relevant to retrieval phase
+#   *Drop all rows where phase_id is not recog_random or recog_ordered
+#   *Drop NaN values
+# Visualizing the data in a table:
+
+#seperate into two tables by phase_id
+#you should be able to drop public ID
+#trying to look at all responses for a single video
+# rename response column and stim_type name to indicate video
+#drop mascot name and vid context
+
+#createa a function that will repeat this for all mascots
+# create a function that will add these new dfs as columns to create a new table.
+
+# determine whether response is a Hit or False Alarm
+#   *create an empty column
+#   *when studied and sure/maybe/guess old, = HIT in a new column
+#   *when lure and sure/maybe/guess new, = HIT in a new column
+#   *when studied and sure/maybe/guess new, = False alarm in a new column
+#   *when lure and sure/maybe/guess old, = False alarm in a new column
+#   *alternatively, could this become a boolean mask where TRUE = hit?
+
 #Create a new table from data: columns: individual video, row: participant response
 #Determine the number of participant responses per mascot video
 #Identify the number of “Hit” per mascot video
