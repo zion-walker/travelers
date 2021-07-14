aa9d257bed6378802f0f491cab492f55c081e4a1
zion-walker <74158727+zion-walker@users.noreply.github.com>
2021-07-14 15:53:17 -0400

    Add files via upload

--- seqlearn_LMEM.R
+++ seqlearn_LMEM.R
@@ -12,7 +12,17 @@ summary(LMEM_mascot)
 
 LMEM_phase = lm(Hit_Rate ~  phase_id, data = LMEM_data)
 summary(LMEM_phase)
+plot(fitted(LMEM_phase),residuals(LMEM_phase))
 
-LMEM_dual = lm(Hit_Rate ~  phase_id, mascot_name, data = LMEM_data)
+
+# Mixed Effect Model 
+LMEM_dual = lm(Hit_Rate ~  phase_id + mascot_name, data = LMEM_data)
 summary(LMEM_dual)
+# Residuals = data point fit to linear model?
+# R^2= how well the model works, AKA variance in pitch accounted for by diff btw M/F 
+# Adjusted R^2 = amount of variance explained given # of “fixed effects”
+# p-value = probability under condition that null hypothesis = true. Low=low probability of no effect.
+# In this case, null=“sex has no effect on pitch”
+# Note overall p-value = p-value for “sexmale” because used only one fixed effect
+# If multiple fixed effects, overall p-val != pval sexmale 
 
